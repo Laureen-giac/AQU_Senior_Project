3d7a5148ee555039fdc2e1e85088701f42b31f92
Loreen Giac <laureen.giac@gmail.com>
2018-01-22 22:13:37 +0200

    Update ctrl_burst_cas.sv

--- ctrl_burst_cas.sv
+++ ctrl_burst_cas.sv
@@ -15,9 +15,11 @@
 module ctrl_burst_cas(ctrl_interface ctrl_intf, ddr_interface ddr_intf);
 
   cas_fsm_type cas_state, cas_next_state;
-  bit clear_rc_counter, trcd_done, clear_cas_counter, tccd_done, clear_extra_counter, next_cas;
-  int rc_counter, cas_counter, extra_wait, extra_counter;
-  logic[2:0]request,  prev_req, act_cmd_trk[$];
+  bit clear_cas_counter, clear_extra_counter, next_cas, rtw, ignore;
+  int rc_counter, cas_counter, extra_wait, extra_counter, cas_delay;
+  int act_rw_trk[$]; 
+  logic[2:0]request,  prev_req; 
+  logic[2:0] act_cmd_trk[$];
 
   always_ff@(posedge ddr_intf.CK_t or negedge ddr_intf.reset_n)
     begin
@@ -36,21 +38,26 @@ module ctrl_burst_cas(ctrl_interface ctrl_intf, ddr_interface ddr_intf);
         begin
           cas_next_state <= CAS_IDLE;
           clear_extra_counter <= 1'b1;
-          clear_rc_counter <= 1'b1;
           clear_cas_counter <= 1'b1;
           ctrl_intf.cas_rdy <= 1'b0;
           ctrl_intf.cas_idle <= 1'b1;
           prev_req <= RD_R; 
+          ignore = 1'b0;
         end
       else
         begin
           case(cas_state)
             CAS_IDLE: begin
               clear_extra_counter <= 1'b1;
-              clear_rc_counter <= 1'b1;
           	  clear_cas_counter <= 1'b1;
           	  ctrl_intf.cas_rdy <= 1'b0;
               ctrl_intf.cas_idle <= 1'b1;
+              
+              if(!ignore)
+                begin
+                  prev_req <= ctrl_intf.act_rw;
+                  ignore = 1'b1;
+                end 
 
               /* Wait for an ACTIVATE. When skipped?
               *********CHECK BURST_ACT**************
@@ -64,10 +71,10 @@ module ctrl_burst_cas(ctrl_interface ctrl_intf, ddr_interface ddr_intf);
             end
 
             CAS_WAIT_ACT: begin
-              clear_rc_counter <= 1'b0;
-              if(trcd_done)
+              clear_cas_counter <= 1'b0;
+              if(cas_counter == cas_delay)
                 begin
-                  clear_rc_counter <= 1'b1;
+                  clear_cas_counter <= 1'b1;
                   if ((request == prev_req) ||
                       (request  == RD_R && prev_req == RDA_R) ||
                       (request == RDA_R && prev_req == RD_R) ||
@@ -83,25 +90,40 @@ module ctrl_burst_cas(ctrl_interface ctrl_intf, ddr_interface ddr_intf);
                     end
                   else
                     begin
-                      cas_next_state <= CAS_EXTRA_WAIT;
                       get_extra_wait(.prev_req(prev_req),
-                                     .req(request),
+                                     .req(ctrl_intf.req),
                                      .extra_wait(extra_wait));
                       prev_req <= request;
-                      clear_extra_counter <= 1'b0;
-                end
-            end
-            end
-
-            CAS_WAIT_CAS: begin
-
-              if(tccd_done)
+                      if(rtw)
+                        begin
+                          cas_next_state <= CAS_WAIT_DATA;
+                        end 
+                      else 
+                        cas_next_state <= CAS_EXTRA_WAIT;  
+                    end 
+                end 
+            end 
+            
+            CAS_WAIT_DATA: begin 
+              clear_extra_counter <= 1'b1; 
+              if(ctrl_intf.rw_done) 
+                begin 
+                  cas_next_state <= CAS_EXTRA_WAIT; 
+                end 
+            end 
+            
+            CAS_EXTRA_WAIT: begin
+              clear_extra_counter <= 1'b0; 
+            
+            if(extra_counter == extra_wait)
                 begin
-                  ctrl_intf.cas_idle <= 1'b0;
-                  clear_cas_counter <= 1'b1;
-                  cas_next_state <= CAS_WAIT_ACT;
+                  clear_extra_counter <= 1'b1;
+                  cas_next_state <= CAS_CMD;
+                  ctrl_intf.cas_req <= request;
+                  ctrl_intf.cas_rdy <= 1'b1;
                 end
-            end
+
+            end     
 
             CAS_CMD: begin
 
@@ -110,7 +132,6 @@ module ctrl_burst_cas(ctrl_interface ctrl_intf, ddr_interface ddr_intf);
               if(next_cas)
                 begin
                   cas_next_state <= CAS_WAIT_CAS;
-                  clear_cas_counter <= 1'b0; 
                 end
 
               else
@@ -118,94 +139,80 @@ module ctrl_burst_cas(ctrl_interface ctrl_intf, ddr_interface ddr_intf);
                   cas_next_state <= CAS_IDLE;
                 end
             end
-
-            CAS_EXTRA_WAIT: begin
-            
-            if(extra_counter == extra_wait)
-                begin
-                  clear_extra_counter <= 1'b1;
-                  cas_next_state <= CAS_CMD;
-                  ctrl_intf.cas_rdy <= 1'b1;
-                  ctrl_intf.cas_req <= request; 
-                end
-
-            end
-
+          
           endcase
 
         end
 
     end
 
+
   //optimize
   always_ff@(posedge ddr_intf.CK_t)
     begin
-      if(clear_rc_counter)
-        begin
-          rc_counter <= 0;
-          trcd_done <= 1'b0;
-        end
-      else
-        begin
-          rc_counter <= rc_counter + 1;
-          if(rc_counter == tRCD)
-            begin
-              trcd_done <= 1'b1;
-        end
-    end
-      
       if (clear_cas_counter == 1'b1)
        	  cas_counter <= 0;
       else
         begin 
           cas_counter <= cas_counter + 1;
-          if(cas_counter == ctrl_intf.tCCD)
-            begin 
-              tccd_done <= 1'b1; 
-            end
         end 
-
-      if (clear_extra_counter == 1'b1)
-       	  extra_counter <= 0;
-      else
-        extra_counter <= extra_counter + 1;
-	end
-
-  //
-
-
-  always@(ddr_intf.reset_n, ctrl_intf.act_rdy, ctrl_intf.no_act_rdy, next_cas)
-    begin
-      if(!ddr_intf.reset_n)
-        begin
-          act_cmd_trk.delete();
-          request <= NOP_R; 
-        end
-
-      else
-        begin
-          if(((ctrl_intf.act_rdy) || (ctrl_intf.no_act_rdy))
-             && ((cas_state == CAS_IDLE) || (cas_state == CAS_CMD)))
-            begin
-              request = ctrl_intf.act_rw;           
-            end
-          
-          if ((cas_state == CAS_CMD) && (next_cas)) begin
-             request =  act_cmd_trk.pop_front();
-             
-              
-           end
-    end
     end 
   
+  always_ff@(posedge ddr_intf.CK_t)
+    begin
+      if (clear_extra_counter == 1'b1)
+        extra_counter <= 0;
+      else
+        extra_counter <= extra_counter + 1;
+    end
+
+  //
+  
+  always @ (ddr_intf.reset_n, ctrl_intf.act_rdy, ctrl_intf.no_act_rdy, next_cas)
+    begin
+      int temp;
+      if (!ddr_intf.reset_n) 
+        begin
+          act_cmd_trk.delete();
+     	  act_rw_trk.delete();
+          cas_delay = 0;
+          request   = '0;
+        end
+      
+      if (((cas_state == CAS_IDLE) || (cas_state == CAS_CMD)) && 
+       ((ctrl_intf.act_rdy)     || (ctrl_intf.no_act_rdy))) 
+        begin
+          cas_delay = tRCD - 1;
+          request   = ctrl_intf.act_rw;
+        end 
+      
+      else if ((cas_state == CAS_CMD) && (next_cas)) 
+        begin
+          temp = (tRCD - act_cmd_trk.pop_front - 1);
+          
+          if (temp > ctrl_intf.tCCD)
+            cas_delay = temp;
+          else
+            cas_delay = ctrl_intf.tCCD;
+          
+          request   = act_rw_trk.pop_front;
+     
+          foreach (act_cmd_trk[i]) 
+            act_cmd_trk [i] = {(act_cmd_trk[i] + cas_delay +1 )};
+        end
+    end
+  
   always @(ctrl_intf.act_rdy, ctrl_intf.no_act_rdy)
-    begin   
+    begin  
       
       if (((ctrl_intf.act_rdy) || (ctrl_intf.no_act_rdy))  && 
-       ((cas_state != CAS_IDLE)  && (cas_state != CAS_CMD))) begin
-        act_cmd_trk.push_back(ctrl_intf.act_rw);  
-      end 
+       ((cas_state != CAS_IDLE)  && (cas_state != CAS_CMD))) 
+        begin
+          act_cmd_trk.push_back(ctrl_intf.act_rw);
+          act_rw_trk.push_back(cas_delay - cas_counter); 
+        end       
     end 
+
 
   always_ff@(posedge ddr_intf.CK_t)
     begin
@@ -229,9 +236,14 @@ module ctrl_burst_cas(ctrl_interface ctrl_intf, ddr_interface ddr_intf);
         begin
           //user programmed refer to waveform
           extra_wait = ctrl_intf.CL + (2 * ctrl_intf.AL) + ctrl_intf.BL/2 + ctrl_intf.CWL + 2;
+          rtw = 1'b1; 
         end
       else //Write to Read
-        extra_wait = tWTR + 4;
+        begin 
+          extra_wait = tWTR + 4;
+          rtw = 1'b0; 
+         end 
+      
     end
   endfunction
 
