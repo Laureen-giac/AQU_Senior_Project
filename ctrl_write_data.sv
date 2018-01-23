417887a13bd2321213e184f57172b2f0214997c4
Loreen Giac <laureen.giac@gmail.com>
2018-01-23 11:50:58 +0200

    Update ctrl_write_data.sv

--- ctrl_write_data.sv
+++ ctrl_write_data.sv
@@ -1,6 +1,6 @@
 `include "ddr_pkg.pkg"
 
-module write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_interface tb_intf) ; 
+module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_interface tb_intf) ; 
 
 
   wr_data_type wr_in , wr_out,wr_out_final;  
@@ -17,21 +17,21 @@ module write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_interface
       begin
         wr_in.burst_length = ctrl_intf.BL ;
         wr_in.preamable = ctrl_intf.WR_PRE;
-        wr_in.wr_data = tb_intf.wr_data;  
+        wr_in.wr_data = tb_intf.host_data;  
         wr_queue.push_back(wr_in);
       end 
     
-    if(ctrl_intf.wr_rdy)
+   /* if(ctrl_intf.wr_rdy)
       begin 
          wr_out_final = wr_out ; 
-       end        
+       end*/       
   end
   
    always_ff@(posedge ctrl_intf.wr_rdy)
     begin 
-      wr_out = wr_queue.pop_front();
+      wr_out_final = wr_queue.pop_front();
     end
-
+     
   always_ff @(ddr_intf.CK_t)
      begin
         if ((ctrl_intf.wr_rdy) )begin
@@ -40,21 +40,20 @@ module write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_interface
      
         end
     end
-    
-    
+      
   task set_wr_pins(input wr_data_type rw_D );
      begin 
        
        @(posedge ddr_intf.CK_r);
        ddr_intf.dq = 8'bz ;
        repeat(rw_D.preamable) @(posedge ddr_intf.CK_r); 
-       repeat(rw_D.burst_length)
+       repeat(rw_D.burst_length + 1)
          begin  
            @(posedge ddr_intf.CK_r)
            ddr_intf.dq = rw_D.wr_data[7:0] ;
            rw_D.wr_data=  rw_D.wr_data >> 8 ;
          end 
-    ddr_intf.dq = 8'bz;
+     ddr_intf.dq = 8'bz;
        
      end 
   
@@ -87,7 +86,7 @@ module write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_interface
        repeat(rw_D.burst_length +1 )@(posedge ddr_intf.CK_r); 
        begin
          ddr_intf.dqs_t <= !ddr_intf.dqs_t; 
-     	   ddr_intf.dqs_c <= !ddr_intf.dqs_c;
+     	 ddr_intf.dqs_c <= !ddr_intf.dqs_c;
        end 
        
        ddr_intf.dqs_t <= 1'b1; 
