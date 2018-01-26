048812e0b0476fc49ce0e02fcb173a6ec8532194
Loreen Giac <laureen.giac@gmail.com>
2018-01-26 11:20:09 +0200

    Update environment.sv

--- environment.sv
+++ environment.sv
@@ -1,40 +1,43 @@
 `include "host_req.sv"
 `include "generator.sv"
 `include "driver.sv"
+`include "monitor.sv"
 
 class environment; 
   
   generator gen; 
-  driver drv;
-  environment env; 
+  driver drv; 
+  monitor mon;  
   mailbox gen2dvr; 
+  mailbox mon2ch;
+  event env_stop; 
   virtual tb_interface tb_intf; 
-  int no_reqs; 
-  
   
   
   function new(virtual tb_interface tb_intf); 
     this.tb_intf = tb_intf; 
     gen2dvr = new();  
+    mon2ch = new(); 
     gen = new(.gen2drv(gen2dvr));
     
     drv = new(.gen2dvr(gen2dvr), 
               .tb_intf(tb_intf));
-   
     
+    mon = new(.mon2ch(mon2ch), 
+              .tb_intf(tb_intf)); 
   endfunction 
   
   task run(); 
     fork
       gen.run(1); 
       drv.run();
+      mon.run(); 
     join
-    
-    if(gen.trans == drv.no_trans)
-        begin 
-          $finish; 
-        end 
-    
+  endtask
+  
+  task stop();
+    wait((gen.no_trans == drv.no_trans) && (gen.no_trans == mon.no_trans));
+    $finish; 
   endtask 
   
 
