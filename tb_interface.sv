f23d0ea81b8de9c2bb54ad58b3d896ec220c1770
Loreen Giac <laureen.giac@gmail.com>
2018-01-26 11:10:19 +0200

    Update tb_interface.sv

--- tb_interface.sv
+++ tb_interface.sv
@@ -1,6 +1,6 @@
 `include "ddr_pkg.pkg"
 
-interface tb_interface; 
+interface tb_interface(ddr_interface ddr_intf); 
   
   bit[63:0] rd_data; 
   write_data wr_data; 
@@ -13,9 +13,44 @@ interface tb_interface;
   bit RD_PRE; 
   bit WR_PRE;
   bit mrs_update; 
-  bit cmd_rdy; 
+  bit cmd_rdy;  
+  
+  clocking driver_cb@(posedge ddr_intf.CK_t); 
+    input rd_data; 
+    output cmd_rdy; 
+    output wr_data; 
+    output request; 
+    output log_addr; 
+    output CL; 
+    output AL;
+    output BL; 
+    output CWL; 
+    output RD_PRE; 
+    output WR_PRE; 
+    output mrs_update;  
+  endclocking 
   
   
+  clocking monitor_cb@(posedge ddr_intf.CK_t); 
+    input wr_data; 
+    input cmd_rdy; 
+    input request;
+    input log_addr; 
+    input CL; 
+    input AL; 
+    input BL; 
+    input CWL; 
+    input RD_PRE; 
+    input WR_PRE; 
+    input mrs_update;
+    output rd_data; 
+  endclocking 
+  
+  
+  modport MONITOR(clocking monitor_cb); 
+  
+  modport DRIVER(clocking driver_cb); 
+    
 endinterface 
 
 
