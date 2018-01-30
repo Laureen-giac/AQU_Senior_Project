836de78fb178ee9cdd7980f0bc4b6a51531e8652
Loreen Giac <laureen.giac@gmail.com>
2018-01-30 22:47:06 +0200

    working version

--- ctrl_write_data.sv
+++ ctrl_write_data.sv
@@ -29,16 +29,16 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
      
   always_ff @(ddr_intf.CK_t)
      begin
-        if ((ctrl_intf.wr_rdy) )begin
-          set_diff_dqs(.rw_D(wr_out_final));
-          set_wr_pins (.rw_D(wr_out_final));
+        if ((ctrl_intf.wr_rdy))
+          begin
+            fork
+              set_diff_dqs(.rw_D(wr_out_final));
+         	  set_wr_pins (.rw_D(wr_out_final));
+            join 
      
         end
     end
   
-  
-  
-      
   task set_wr_pins(input wr_data_type rw_D );
      begin 
        
@@ -56,27 +56,6 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
      end 
   
   endtask 
-  
-  
-  
-  
- /*    
-  task set_read_data_pins (input wr_data_type rw_D);
-    begin 
-      @(posedge ddr_intf.CK_r); 
-      DQ_BUS = 8'bz ;
-      repeat(rw_D.preamable) @(posedge ddr_intf.CK_r); 
-      repeat(rw_D.burst_length)
-        begin  
-          @(posedge ddr_intf.CK_r)
-          DQ_BUS = rw_D.wr_data[7:0];
-          rw_D.wr_data=  rw_D.wr_data >> 8;
-        end 
-     DQ_BUS = 8'bz; 
-    end 
-  
-  endtask 
- */    
      
   task set_diff_dqs(input wr_data_type rw_D);
      begin
@@ -94,8 +73,8 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
          @ (posedge ddr_intf.CK_r)
      ddr_intf.dqs_t = ~ddr_intf.dqs_t;
      ddr_intf.dqs_c =1'b1;
-   end  
-       
+   end;  
+      
        ddr_intf.dqs_t <= 1'b1; 
        ddr_intf.dqs_c <= 1'b1; 
      end
