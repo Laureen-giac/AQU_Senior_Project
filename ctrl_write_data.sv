451c7bd3959a32f0a8033a50c897095698ee1359
Loreen Giac <laureen.giac@gmail.com>
2018-02-01 13:35:27 +0200

    Update ctrl_write_data.sv

--- ctrl_write_data.sv
+++ ctrl_write_data.sv
@@ -15,22 +15,26 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
     
     if(ctrl_intf.act_rdy || ctrl_intf.no_act_rdy )
       begin
-        wr_in.burst_length = ctrl_intf.BL ;
-        wr_in.preamable = ctrl_intf.WR_PRE;
-        wr_in.wr_data = tb_intf.wr_data;  
-        wr_queue.push_back(wr_in);
-      end     
-  end
+        if(tb_intf.wr_data != '1)
+          begin 
+            wr_in.burst_length = ctrl_intf.BL ;
+            wr_in.preamable = ctrl_intf.WR_PRE;
+            wr_in.wr_data = tb_intf.wr_data;  
+            wr_queue.push_back(wr_in);
+         end
+      end 
+     end
   
    always_ff@(posedge ctrl_intf.wr_rdy)
     begin 
-      wr_out_final = wr_queue.pop_front();
+      wr_out = wr_queue.pop_front();
     end
      
   always_ff @(ddr_intf.CK_t)
      begin
         if ((ctrl_intf.wr_rdy))
           begin
+            wr_out_final = wr_out;
             fork
               set_diff_dqs(.rw_D(wr_out_final));
          	  set_wr_pins (.rw_D(wr_out_final));
@@ -41,21 +45,19 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
   
   task set_wr_pins(input wr_data_type rw_D );
      begin 
-       
        @(negedge  ddr_intf.CK_r);
-       ddr_intf.dq = 8'bz ;
+       ddr_intf.dq = 'z ;
        repeat(rw_D.preamable) @(negedge ddr_intf.CK_r); 
        repeat(rw_D.burst_length + 1)
          begin  
            @(negedge ddr_intf.CK_r)
-           ddr_intf.dq = rw_D.wr_data[7:0] ;
-           rw_D.wr_data=  rw_D.wr_data >> 8 ;
+           ddr_intf.dq = rw_D.wr_data[7:0]; 
+           rw_D.wr_data=  {8'h00, rw_D.wr_data[63:8]} ;
          end 
-     ddr_intf.dq = 8'bz;
-       
+     ddr_intf.dq = 'z;
      end 
-  
   endtask 
+ 
      
   task set_diff_dqs(input wr_data_type rw_D);
      begin
