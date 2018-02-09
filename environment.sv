6dc453732d8d889d9caea74dac2e750d92872b9f
Loreen Giac <laureen.giac@gmail.com>
2018-02-09 14:38:01 +0200

    Update environment.sv

--- environment.sv
+++ environment.sv
@@ -2,44 +2,66 @@
 `include "generator.sv"
 `include "driver.sv"
 `include "monitor.sv"
+//`include "scoreboard.sv"
 
 class environment; 
   
   generator gen; 
   driver drv; 
   monitor mon;  
+  //scoreboard sb; 
   mailbox gen2dvr; 
-  mailbox mon2ch;
+  mailbox gen2sb; 
+  mailbox mon2sb;
   event env_stop; 
   virtual tb_interface tb_intf; 
+  virtual ddr_interface ddr_intf; 
+  virtual ctrl_interface ctrl_intf;
   
   
-  function new(virtual tb_interface tb_intf); 
+  function new(input virtual tb_interface tb_intf, input virtual ddr_interface ddr_intf, input virtual ctrl_interface ctrl_intf); 
     this.tb_intf = tb_intf; 
-    gen2dvr = new();  
-    mon2ch = new(); 
-    gen = new(.gen2drv(gen2dvr));
+    this.ddr_intf = ddr_intf; 
+    this.ctrl_intf = ctrl_intf; 
+    
+    gen2dvr = new(); 
+    gen2sb = new(); 
+    mon2sb = new(); 
+    
+    gen = new(.gen2drv(gen2dvr), 
+              .gen2sb(gen2sb));
     
     drv = new(.gen2dvr(gen2dvr), 
+              .tb_intf(tb_intf), 
+              .ddr_intf(ddr_intf),
+              .ctrl_intf(ctrl_intf));
+    
+    mon = new(.mon2sb(mon2sb), 
               .tb_intf(tb_intf));
     
-    mon = new(.mon2ch(mon2ch), 
-              .tb_intf(tb_intf)); 
+  //  sb = new(.mon2sb(mon2sb), 
+            // .tb_intf(tb_intf)); 
+    
   endfunction 
+  
+  task reset();
+    begin 
+      drv.reset();
+    end 
+  endtask 
   
   task run(); 
     fork
-      gen.run(1); 
-      drv.run();
-      mon.run(); 
+      gen.run(7); 
+      drv.run(7);
+      mon.run(7); 
+     //sb.main(5); 
     join
   endtask
   
   task stop();
-    wait((gen.no_trans == drv.no_trans) && (gen.no_trans == mon.no_trans));
-    $finish; 
+    #5ns $finish;
   endtask 
   
 
 endclass 
-  
