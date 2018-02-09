f57f98610bd69272a9d5c4eed3c0eea811b65327
Loreen Giac <laureen.giac@gmail.com>
2018-02-09 15:21:41 +0200

    Update tb_top.sv

--- tb_top.sv
+++ tb_top.sv
@@ -6,34 +6,29 @@
 
 module tb_top;
   
-  ddr_interface ddr_intf(); 
-  tb_interface tb_intf(ddr_intf);
+  ddr_interface ddr_intf();
   ctrl_interface ctrl_intf();
-  test t(tb_intf);
+  tb_interface tb_intf(ddr_intf, ctrl_intf); 
+  test test1(tb_intf, ddr_intf, ctrl_intf);
   
-  initial 
-    begin
-      ddr_intf.reset_n <= 1'b1;
-      tb_intf.cmd_rdy <= 1'b0;
-      #200ns
-      @(negedge ddr_intf.CK_t);
-      ddr_intf.reset_n <= 1'b0;
-      #200us
-      ddr_intf.reset_n <= 1'b1;
-      #300ns
-      @(negedge ddr_intf.CK_t)
-      tb_intf.cmd_rdy<= 1'b1;
-      $monitor("DDR4 RW state is %s\n", DUT.rw.current_rw_state.name());
-      @(negedge ddr_intf.CK_t)
-      tb_intf.cmd_rdy <= 1'b0;
-      #3ns
-      tb_intf.cmd_rdy <= 1'b1;
-    end 
-  
+   initial begin
+    @(negedge ddr_intf.CK_t);
+    ddr_intf.reset_n <= 1'b0;
+    #200us
+    ddr_intf.reset_n <= 1'b1; 
+  end
+ 
   top DUT(.ctrl_intf(ctrl_intf),
-            .ddr_intf(ddr_intf),
-            .tb_intf(tb_intf));
+          .ddr_intf(ddr_intf),
+          .tb_intf(tb_intf));
   
   
 endmodule 
- 
+  
+  
+      
+  
+  
+  
+  
+
