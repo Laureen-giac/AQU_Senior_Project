d1b0566051c4d129bb399785f3ebcf80993000cd
Loreen Giac <laureen.giac@gmail.com>
2018-02-15 10:47:10 +0200

    Update tb_top.sv

--- tb_top.sv
+++ tb_top.sv
@@ -12,11 +12,16 @@ module tb_top;
   test test1(tb_intf, ddr_intf, ctrl_intf);
   
    initial begin
-    @(negedge ddr_intf.CK_t);
-    ddr_intf.reset_n <= 1'b0;
-    #200us
-    ddr_intf.reset_n <= 1'b1; 
-  end
+     @(negedge ddr_intf.CK_t);
+     ddr_intf.reset_n <= 1'b0;
+     #200us
+     ddr_intf.reset_n <= 1'b1; 
+     //$monitor("act_idle is %h at %0gns", ctrl_intf.act_idle, $time); 
+    // $monitor("dimm_req %h", tb_intf.cmd_rdy); 
+    //$monitor("index has %h", ddr_intf.data_out.wr_data);
+    // $monitor("%h", ddr_intf.data_out.wr_data); 
+     
+   end
  
   top DUT(.ctrl_intf(ctrl_intf),
           .ddr_intf(ddr_intf),
