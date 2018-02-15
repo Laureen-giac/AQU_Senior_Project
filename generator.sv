a06a7110848a0a7a50578a9528221511c18ecdaf
Loreen Giac <laureen.giac@gmail.com>
2018-02-15 10:50:45 +0200

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -1,34 +1,41 @@
 class generator; 
   
-  host_request gen_req;
-  mailbox gen2driv; 
-  int no_reqs; 
-  int trans = 0; 
+  host_req gen_req;
+  mailbox gen2drv; 
+  mailbox gen2sb; 
+  int no_trans = 0; 
   
- function new(input mailbox gen2driv,  
-               input  no_reqs);
-   this.gen2driv = gen2driv;  
-   this.no_reqs = no_reqs;
+  function new(input mailbox gen2drv, mailbox gen2sb);
+   this.gen2drv = gen2drv; 
+   this.gen2sb = gen2sb; 
  endfunction
   
-  task run; 
+  task run(input int no_reqs); 
     repeat(no_reqs) 
       begin 
-        gen_req = new(); 
+        gen_req = new();
         if(gen_req.randomize()) begin 
-          $display("-----------------------------"); 
-          $display("@%0t:Gen%0d", $time, trans); 
-          $display("Host Address:%0h\nRequest:%0h\nWrite Data:%0h\n",gen_req.log_addr, gen_req.request, gen_req.data.wr_data);
-          gen2driv.put(gen_req); 
-          trans++;
+         // $display("-----------------------------"); 
+         // $display("@%0t:Gen%0d", $time, no_trans); 
+         // $display("Host Address:%0h\nRequest:%0h\nWrite Data:%0h\n",gen_req.log_addr, gen_req.request, gen_req.wr_data);
+          gen2drv.put(gen_req); 
+          gen2sb.put(gen_req); 
+          no_trans++;
+          //$display("%d", no_trans); 
         end 
         else 
           begin 
             $fatal("Randomization failed");
           end 
-      end 
-  endtask 
+      end
+  endtask
   
 endclass 
-
+  
+  
+  
+               
+  
+  
+  
   
