9fe7daae61bb33582f937aa8150a9bf5ea005106
Loreen Giac <laureen.giac@gmail.com>
2018-02-25 23:06:57 +0200

    Update ctrl_write_data.sv

--- ctrl_write_data.sv
+++ ctrl_write_data.sv
@@ -19,8 +19,9 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
           begin 
             wr_in.burst_length = ctrl_intf.BL ;
             wr_in.preamable = ctrl_intf.WR_PRE;
-            wr_in.wr_data = tb_intf.wr_data;  
+            wr_in.wr_data = tb_intf.wr_data; 
             wr_queue.push_back(wr_in);
+           // $display("the ctrl_intf.BLis %d",ctrl_intf.BL);
          end
       end 
      end
@@ -28,15 +29,20 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
    always_ff@(posedge ctrl_intf.wr_rdy)
     begin 
       wr_out = wr_queue.pop_front();
+    // $display("the  wr_out is %h ",wr_out );
     end
      
   always_ff @(ddr_intf.CK_t)
      begin
         if ((ctrl_intf.wr_rdy))
           begin
+            wr_out_final = wr_out;
+          //  $display("the  wr_out is %h ",wr_out_final );
             fork
-              set_diff_dqs(.rw_D(wr_out));
-         	  set_wr_pins (.rw_D(wr_out));
+              set_diff_dqs(.rw_D(wr_out_final));
+         	  set_wr_pins (.rw_D(wr_out_final));
+              //$display("wr_out_final %h\n", wr_out_final);
+              
             join 
      
         end
@@ -44,6 +50,7 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
   
   task set_wr_pins(input wr_data_type rw_D );
      begin 
+    //$display("the  rw_D is %h ",rw_D ); 
        @(negedge  ddr_intf.CK_r);
        ddr_intf.dq = 'z ;
        repeat(rw_D.preamable) @(negedge ddr_intf.CK_r); 
@@ -51,7 +58,9 @@ module ctrl_write_data(ctrl_interface ctrl_intf, ddr_interface ddr_intf, tb_inte
          begin  
            @(negedge ddr_intf.CK_r)
            ddr_intf.dq = rw_D.wr_data[7:0]; 
-           rw_D.wr_data=  rw_D.wr_data >> 8 ;
+           //$display("the  ddr_intf.dq is %h ",ddr_intf.dq ); 
+           rw_D.wr_data=  {8'h00, rw_D.wr_data[63:8]} ;
+          
          end 
      ddr_intf.dq = 'z;
      end 
