5c1f96d4a2b9b2ce51dd079d3d1d854f4091dfdd
Loreen Giac <laureen.giac@gmail.com>
2018-03-02 13:13:04 +0200

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -23,7 +23,7 @@ class driver;
   
   task reset(); 
     wait(ddr_intf.reset_n);
-    `DRIVER.driver_cb.BL <= 2'b00;
+    `DRIVER.driver_cb.BL <= $urandom_range(2'b00, 2'b10);
     `DRIVER.driver_cb.CL <= 3'b010;
     `DRIVER.driver_cb.AL <= 2'b11;
     `DRIVER.driver_cb.CWL <= 3'b000;
@@ -37,23 +37,18 @@ class driver;
   /* Get requests from generator, and send to DUT
   */
   task run();
-    forever
-      begin  
-        host_req gen_req;
-        tb_intf.cmd_rdy <=  1'b0;
-        wait(tb_intf.rw_proc); 
-        @(posedge ddr_intf.CK_t);
-        if(ctrl_intf.act_idle && !ctrl_intf.busy) begin
-          if(gen2dvr.num() != 0) begin 
-            #900ns;
-            tb_intf.cmd_rdy <= 1'b1; 
-            @(posedge ddr_intf.CK_t);  
-            gen2dvr.get(gen_req);  
-           `DRIVER.driver_cb.log_addr <= gen_req.log_addr;
-           `DRIVER.driver_cb.request <= gen_req.request;
-           `DRIVER.driver_cb.wr_data <= gen_req.wr_data;
-            @(posedge ddr_intf.CK_t);
-           // $display("DRIVER%0d:: Host Address:%0h\nRequest:%0h\nWrite Data:%0h\n",no_trans, gen_req.log_addr[27:0], gen_req.request, gen_req.wr_data);
+    forever begin
+      host_req gen_req;
+      @(posedge ddr_intf.CK_t);
+      if(ctrl_intf.rw_idle && !ctrl_intf.busy) begin
+        if(gen2dvr.num() != 0) begin 
+          tb_intf.cmd_rdy <= 1'b1; 
+          gen2dvr.get(gen_req); 
+          `DRIVER.driver_cb.log_addr <= gen_req.log_addr;
+          `DRIVER.driver_cb.request <= gen_req.request;
+          `DRIVER.driver_cb.wr_data <= gen_req.wr_data;
+          @(posedge ddr_intf.CK_t);
+           //$display("DRIVER%0d:: Host Address:%0h\nRequest:%0h\nWrite Data:%0h\n",no_trans, gen_req.log_addr[27:0], gen_req.request, gen_req.wr_data);
             tb_intf.cmd_rdy <=  1'b0;
             no_trans++;
             #30ns;
