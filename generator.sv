ebae615091854ad88edc295807bb959f31db241c
Loreen Giac <laureen.giac@gmail.com>
2018-04-07 12:45:20 +0300

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -5,6 +5,7 @@ class generator;
   mailbox gen2sb; 
   host_address addr_queue[$];  
   int no_trans = 0; 
+  event ended; 
   
   function new(input mailbox gen2drv, mailbox gen2sb);
    this.gen2drv = gen2drv; 
@@ -21,16 +22,16 @@ class generator;
             gen_req.wr_data = $random(); 
           end 
           if(gen_req.request == WR_R) begin 
-            addr_queue.push_back(gen_req.log_addr); 
+            addr_queue.push_back(gen_req.phy_addr); 
           end 
           else if(no_trans != 0 && gen_req.request == RD_R) begin 
             if(addr_queue.size() != 0) begin 
-              gen_req.log_addr = addr_queue.pop_front();
+              gen_req.phy_addr = addr_queue.pop_front();
             end 
           end 
          // $display("-----------------------------"); 
          // $display("@%0t:Gen%0d", $time, no_trans); 
-            $display("Host Address:%0h\nRequest:%0h\nWrite Data:%0h\n",gen_req.log_addr, gen_req.request, gen_req.wr_data);
+          // $display("Host Address:%0h\nRequest:%0h\nWrite Data:%0h\n",gen_req.phy_addr, gen_req.request, gen_req.wr_data);
           gen2drv.put(gen_req); 
           gen2sb.put(gen_req); 
           no_trans++;
@@ -39,9 +40,9 @@ class generator;
           $fatal("Randomization failed");
         end 
       end
+    ->ended; 
   endtask
   
 endclass 
-  
   
   
